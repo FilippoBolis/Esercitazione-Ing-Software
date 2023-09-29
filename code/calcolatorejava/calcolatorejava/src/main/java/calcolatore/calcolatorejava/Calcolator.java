14907245dac498d8ee1af5777de4fae3d46914f3
Mazzo-Pazzo02 <146365387+Gabriele-Mazzoleni@users.noreply.github.com>
2023-09-29 13:07:50 +0200

    Update Calcolator.java

--- code/calcolatorejava/calcolatorejava/src/main/java/calcolatore/calcolatorejava/Calcolator.java
+++ code/calcolatorejava/calcolatorejava/src/main/java/calcolatore/calcolatorejava/Calcolator.java
@@ -2,12 +2,13 @@ package calcolatore.calcolatorejava;
 
 public class Calcolator {
 	public Calculator(){
-	int A = 0;
-	int B = 0;
 	}
 
 //Qui MazzoPazzo metti la somma 
-
+	public int sum(int a, int b) {
+		int s=a+b;
+		return s;
+	}
 //Qui IngegnerGotti metta pure la differenza
 	
 	
