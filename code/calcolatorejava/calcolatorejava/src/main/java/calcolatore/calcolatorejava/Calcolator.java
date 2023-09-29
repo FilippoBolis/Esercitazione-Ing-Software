6690829b8375c3ea1b4ddae1abc25ae24c9794a7
FilippoBolis <145765573+FilippoBolis@users.noreply.github.com>
2023-09-29 13:01:48 +0200

    Update Calcolator.java

--- code/calcolatorejava/calcolatorejava/src/main/java/calcolatore/calcolatorejava/Calcolator.java
+++ code/calcolatorejava/calcolatorejava/src/main/java/calcolatore/calcolatorejava/Calcolator.java
@@ -1,6 +1,14 @@
 package calcolatore.calcolatorejava;
 
 public class Calcolator {
+	public Calculator(){
+	int A = 0;
+	int B = 0;
+	}
+
+//Qui MazzoPazzo metti la somma 
+
+//Qui IngegnerGotti metta pure la differenza
 	
 	
 }
